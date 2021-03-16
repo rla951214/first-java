bd21c8723fa9e0dc86864553c998bf88a7fe490d
rla951214 <rla951214@naver.com>
2021-03-16 15:31:01 +0900

    first commit

---
+++ src/main/java/study04/Theory1.java
@@ -0,0 +1,15 @@
+package study04;
+
+public class Theory1 {
+
+    /*
+    * 문자열 : 문자의 열, 문자가 나열되어 있는 것, 'a' "abc"
+    * String str = scan.nextLine();
+    * String str = "hello"
+    */
+
+    public static void main(String[] args) {
+        int number = 10;
+        System.out.println(number);
+    }
+}
